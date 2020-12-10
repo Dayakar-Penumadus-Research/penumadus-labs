eae40282fdd3f29162e4792fe99e4ea83be9ba01
Abe <abramabradley@gmail.com>
2020-12-10 01:28:55 -0500

    styling...

--- public/js/style.js
+++ public/js/style.js
@@ -9,9 +9,9 @@ export default () => {
   applyStylesToChildren(main, 'card', 'contained')
   applyStylesToChildren(footer, 'contained')
 
-  for (const img of document.querySelectorAll('img:not(footer img)')) {
-    img.style.width = '800px'
-  }
+  // for (const img of document.querySelectorAll('img:not(footer img)')) {
+  //   img.style.width = '800px'
+  // }
 
   // display everything once everything is rendered
   document.body.style.visibility = 'visible'
